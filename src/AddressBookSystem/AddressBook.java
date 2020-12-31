e4188dfd4a828e3d48538468fbc58ce34cfad4dd
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-31 18:51:06 +0530

    ADDED: address book to remote

--- src/AddressBookSystem/AddressBook.java
+++ src/AddressBookSystem/AddressBook.java
@@ -1,41 +1,9 @@
 package AddressBookSystem;
 
-import java.util.ArrayList;
-
- class AddressBook1 {
-	 public final String fisrtName;
-	   public final String lastName;
-	   public final String state;
-	   public final int zip;
-	   public final int number;
-
-	   public AddressBook1(String firstName, String lastName, String state, int zip, int number)
-	   {
-	      this.fisrtName = firstName;
-	      this.lastName = lastName;
-	      this.state = state;
-	      this.zip = zip;
-	      this.number=number;
-	   }
- }	   
-public class AddressBook
+public class AddressBook 
 {
-	 static ArrayList<AddressBook1> addressBookArrayList;
-	public AddressBook() {
-	  	   addressBookArrayList = new ArrayList<>();
-	}  
-	   public void addToAddressBook1(String firstName, String lastName, String state, int zip, int number) 
-		{
-			AddressBook1 freshEntry = new AddressBook1(firstName, lastName, state, zip, number);
-			addressBookArrayList.add(freshEntry);
-		}
-		public static void main(String[] args) {
-			System.out.println("welcome to address book");
-			AddressBook abc = new AddressBook(); 
-			abc.addToAddressBook1("vin", "rya", "ts", 1, 2);
-			abc.addToAddressBook1("vina", "ryak", "tgs", 11, 21);
-			
-			
-		}
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Address Book Program");
+	}
 }
-	   

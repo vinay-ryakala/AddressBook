082ce681f9897f2a6755c8084de8b6135eefce16
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-31 19:56:24 +0530

    ADDED:To add a new contact to address book UC2

--- src/AddressBookSystem/AddressBook.java
+++ src/AddressBookSystem/AddressBook.java
@@ -1,9 +1,12 @@
 package AddressBookSystem;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook 
 {
+	public static ArrayList<Person> contacts = new ArrayList<Person>();
+	
 	private static void addContact() 
 	{
 		Scanner inp = new Scanner(System.in);
@@ -25,10 +28,44 @@ public class AddressBook
 		Address personAddress = new Address(city,state,zipCode,email);
 		Person details = new Person(firstName, lastName, phoneNum, personAddress);
 		System.out.println(details.toString());
-	}		
+		contacts.add(details);
+	}	
+	private static void display() 
+	{
+		for (Person person : contacts) {
+			System.out.println(person.toString());
+		}
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Address Book Program");
-		addContact();
+		int cond = 1;
+		while (cond == 1) 
+		{
+			System.out.println("Address Book:\n"
+					+ "1) Add Person\n"
+					+ "2) Display\n"
+					+ "0) Close");
+			Scanner inp = new Scanner(System.in);
+			System.out.println("Select an option:");
+			int num=inp.nextInt();
+			switch (num) 
+			{
+			case 1:
+				addContact();
+				break;
+			case 2:
+				display();
+				break;
+			case 0:
+				cond=0;
+				break;
+			default:
+				System.out.println("Wrong option");
+				break;
+			}
+		}
+
 	}
 }
